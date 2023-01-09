3e5276c504844114be3bb13e88cc7a16a7151c76
Rob <robzlegz@gmail.com>
2023-01-09 09:59:43 +0200

    files

--- v1/scripts/split_audio.py
+++ v1/scripts/split_audio.py
@@ -7,7 +7,7 @@ work_dir = os.path.dirname(os.path.dirname(__file__))
 
 sound = AudioSegment.from_mp3(f"{work_dir}/scripts/allsounds.mp3")
 
-audio_chunks = split_on_silence(sound, min_silence_len=200, silence_thresh=-40 )
+audio_chunks = split_on_silence(sound, min_silence_len=100, silence_thresh=-40 )
 
 for i, chunk in enumerate(audio_chunks):
    soundn = sound_names[i]
